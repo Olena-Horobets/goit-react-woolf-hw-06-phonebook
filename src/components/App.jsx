126a52b767ff8d27ce84817c26a4ec13c8588b7f
Olena-Horobets <80359219+Olena-Horobets@users.noreply.github.com>
2024-05-13 13:24:57 -0230

    form state updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,41 +2,13 @@ import Filter from './Filter';
 import Form from './Form';
 import ContactsList from './ContactsList';
 import { useDispatch, useSelector } from 'react-redux';
-import { addContactAction, removeContactAction } from 'store/contacts/slice';
+import { removeContactAction } from 'store/contacts/slice';
 
 function App() {
   const contacts = useSelector(state => state.contacts);
   const filter = useSelector(state => state.filter);
 
   const dispatch = useDispatch();
-  const addContact = addContactAction;
-
-  const onFormSubmit = data => {
-    if (!data.name.trim()) {
-      const newName = prompt(
-        'Name cannot be empty. Please, enter the name below'
-      );
-      data.name = newName;
-    }
-
-    const savedContact = contacts.find(el => el.number === data.number);
-    if (savedContact) {
-      return alert(
-        `This number is already saved under "${savedContact.name}" name`
-      );
-    }
-
-    if (
-      contacts.some(el => el.name.toLowerCase() === data.name.toLowerCase())
-    ) {
-      const newName = prompt(
-        'This name is already used. Please, use different name'
-      );
-      data.name = newName;
-    }
-
-    dispatch(addContact(data));
-  };
 
   const getVisibleContacts = () => {
     if (filter) {
@@ -56,7 +28,7 @@ function App() {
 
   return (
     <div className="container">
-      <Form onSubmit={onFormSubmit}></Form>
+      <Form></Form>
       <Filter />
       {contactList.length ? (
         <ContactsList
